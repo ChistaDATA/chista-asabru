44af2d78e4370dd5d52039a054b88b083770b373
josephabraham <joseph.abraham@chistadata.com>
2023-10-30 21:39:22 +0530

    ssl related changes- initial checkin

---
+++ src/pipelines/ClickHouseTLSPipeline.cpp
@@ -0,0 +1,124 @@
+#include "CProtocolSocket.h"
+#include "CProxySocket.h"
+#include "CClientSocket.h"
+#include "../config/ConfigSingleton.h"
+#include "Socket.h"
+#include "SSLSocket.h"
+#include "SocketSelect.h"
+#include "CHttpParser.h"
+
+#include <utility>
+
+static ConfigSingleton &configSingleton = ConfigSingleton::getInstance();
+
+void *ClickHouseTLSPipeline(CProxySocket *ptr, void *lptr)
+{
+    std::cout << "ClickHouseTLSPipeline::start" << std::endl;
+    CLIENT_DATA clientData;
+    memcpy(&clientData, lptr, sizeof(CLIENT_DATA));
+
+    // Check if handler is defined
+    CProxyHandler *proxy_handler = ptr->GetHandler();
+    if (proxy_handler == 0)
+    {
+        cout << "The handler is not defined. Exiting!" << endl;
+        return 0;
+    }
+
+    /**
+     * Get the configuration data for the target database clusters ( eg. clickhouse )
+     * Config given in config.xml
+     */
+    TARGET_ENDPOINT_CONFIG targetEndpointConfig = ptr->GetConfigValues();
+    int services_count = targetEndpointConfig.services.size();
+    int current_service_index = clientData.current_service_index % services_count;
+    RESOLVED_SERVICE currentService = targetEndpointConfig.services[current_service_index];
+    END_POINT *target_endpoint = new END_POINT{currentService.ipaddress, currentService.port, currentService.r_w, currentService.alias, currentService.reserved, "  "};
+    if (target_endpoint == 0)
+    {
+        cout << "Failed to retrieve target database configuration. Exiting!" << endl;
+        return 0;
+    }
+    cout << "Resolved (Target) Host: " << target_endpoint->ipaddress << endl
+         << "Resolved (Target) Port: " << target_endpoint->port << endl;
+
+    Socket *client_socket = (Socket *)clientData.client_socket;
+    SSLSocket *ssl_client= new SSLSocket(client_socket->GetSocket());
+    CClientSocket *target_socket = new CClientSocket(target_endpoint->ipaddress, target_endpoint->port);
+
+    EXECUTION_CONTEXT exec_context;
+
+    ProtocolHelper::SetReadTimeOut(client_socket->GetSocket(), 1);
+    ProtocolHelper::SetReadTimeOut(target_socket->GetSocket(), 1);
+
+    while (1)
+    {
+        SocketSelect *sel;
+
+        try
+        {
+            sel = new SocketSelect(client_socket, target_socket, NonBlockingSocket);
+        }
+        catch (std::exception &e)
+        {
+            cout << e.what() << endl;
+            cout << "error occurred while creating socket select " << endl;
+        }
+
+        bool still_connected = true;
+        try
+        {
+            if (sel->Readable(client_socket))
+            {
+                cout << "client socket is readable, reading bytes : " << endl;
+                std::string bytes = ssl_client->ReceiveBytes();
+
+                cout << "Calling Proxy Upstream Handler.." << endl;
+                std::string response = proxy_handler->HandleUpstreamData((void *)bytes.c_str(), bytes.size(), &exec_context);
+                target_socket->SendBytes((char *)response.c_str(), response.size());
+
+                if (bytes.empty())
+                    still_connected = false;
+            }
+        }
+        catch (std::exception &e)
+        {
+            cout << "Error while sending to target " << e.what() << endl;
+        }
+
+        try
+        {
+            if (sel->Readable(target_socket))
+            {
+                cout << "target socket is readable, reading bytes : " << endl;
+                std::string bytes = target_socket->ReceiveBytes();
+
+                cout << "Calling Proxy Downstream Handler.." << endl;
+                std::string response = proxy_handler->HandleDownStreamData((void *)bytes.c_str(), bytes.size(), &exec_context);
+                ssl_client->SendBytes((char *)response.c_str(), response.size());
+
+                if (bytes.empty())
+                    still_connected = false;
+            }
+        }
+        catch (std::exception &e)
+        {
+            cout << "Error while sending to client " << e.what() << endl;
+        }
+
+        if (!still_connected)
+        {
+            // Close the client socket
+            ssl_client->Close();
+            break;
+        }
+    }
+
+    // Close the server socket
+    target_socket->Close();
+#ifdef WINDOWS_OS
+    return 0;
+#else
+    return nullptr;
+#endif
+}
