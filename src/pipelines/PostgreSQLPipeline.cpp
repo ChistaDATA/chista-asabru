9d36ec0db06f532fc1cf5b14ee8bc7c4a7eee99b
Midhun A Darvin <midhunadarvin@gmail.com>
2024-01-04 14:47:01 +0530

    fix: Postgres - TLS - Failed to Connect
    - https://github.com/ChistaDATA/chista-asabru/issues/4

--- src/pipelines/PostgreSQLPipeline.cpp
+++ src/pipelines/PostgreSQLPipeline.cpp
@@ -12,6 +12,7 @@
 
 void *PostgreSQLPipeline(CProxySocket *ptr, void *lptr)
 {
+    LOG_INFO("PostgreSQLPipeline::start");
     CLIENT_DATA clientData;
     memcpy(&clientData, lptr, sizeof(CLIENT_DATA));
 
@@ -28,8 +29,8 @@ void *PostgreSQLPipeline(CProxySocket *ptr, void *lptr)
 
     RESOLVED_SERVICE currentService = loadBalancer->requestServer();
     END_POINT target_endpoint = END_POINT{currentService.ipaddress, currentService.port, currentService.r_w, currentService.alias, currentService.reserved, "  "};
-    std::cout << "Resolved (Target) Host: " << target_endpoint.ipaddress << std::endl
-         << "Resolved (Target) Port: " << target_endpoint.port << std::endl;
+    LOG_INFO("Resolved (Target) Host: " + target_endpoint.ipaddress);
+    LOG_INFO("Resolved (Target) Port: " + std::to_string(target_endpoint.port));
 
     Socket *client_socket = (Socket *)clientData.client_socket;
     CClientSocket *target_socket = new CClientSocket(target_endpoint.ipaddress, target_endpoint.port);
@@ -41,19 +42,28 @@ void *PostgreSQLPipeline(CProxySocket *ptr, void *lptr)
     ProtocolHelper::SetReadTimeOut(target_socket->GetSocket(), 1);
     ProtocolHelper::SetKeepAlive(target_socket->GetSocket(), 1);
 
-    while (1)
+    while (true)
     {
+        SocketSelect *sel;
         try
         {
-            SocketSelect sel(client_socket, target_socket, NonBlockingSocket);
-            bool still_connected = true;
+            sel = new SocketSelect(client_socket, target_socket, NonBlockingSocket);
+        }
+        catch (std::exception &e)
+        {
+            LOG_ERROR(e.what());
+            LOG_ERROR("error occurred while creating socket select ");
+        }
 
-            if (sel.Readable(client_socket))
+        bool still_connected = true;
+        try
+        {
+            if (sel->Readable(client_socket))
             {
-                std::cout << "client socket is readable, reading bytes : " << std::endl;
+                LOG_INFO("client socket is readable, reading bytes : ");
                 std::string bytes = client_socket->ReceiveBytes();
 
-                std::cout << "Calling Proxy Upstream Handler.." << std::endl;
+                LOG_INFO("Calling Proxy Upstream Handler..");
                 std::string response = proxy_handler->HandleUpstreamData((void *)bytes.c_str(), bytes.size(), &exec_context);
                 target_socket->SendBytes((char *)response.c_str(), response.size());
                 // target_socket.SendBytes((char *) bytes.c_str(), bytes.size());
@@ -61,34 +71,51 @@ void *PostgreSQLPipeline(CProxySocket *ptr, void *lptr)
                 if (bytes.empty())
                     still_connected = false;
             }
-            if (sel.Readable(target_socket))
+        }
+        catch (std::exception &e)
+        {
+            LOG_ERROR("Error while sending to target " + std::string(e.what()));
+            still_connected = false;
+        }
+
+        try
+        {
+            if (sel->Readable(target_socket))
             {
-                std::cout << "target socket is readable, reading bytes : " << std::endl;
+                LOG_INFO("target socket is readable, reading bytes : ");
                 std::string bytes = target_socket->ReceiveBytes();
 
-                std::cout << "Calling Proxy Downstream Handler.." << std::endl;
+                LOG_INFO("Calling Proxy Downstream Handler..");
                 std::string response = proxy_handler->HandleDownStreamData((void *)bytes.c_str(), bytes.size(), &exec_context);
                 client_socket->SendBytes((char *)response.c_str(), response.size());
 
                 if (bytes.empty())
                     still_connected = false;
             }
-            if (!still_connected)
-            {
-                // Close the client socket
-                client_socket->Close();
-                break;
-            }
         }
         catch (std::exception &e)
         {
-            LOG_ERROR(e.what());
+            LOG_ERROR("Error while sending to client " + std::string(e.what()));
+            still_connected = false;
+        }
+
+        if (!still_connected)
+        {
+            // Delete Select from memory
+            delete sel;
+
+            // Close the client socket
+            client_socket->Close();
+            delete client_socket;
             break;
         }
     }
 
     // Close the server socket
     target_socket->Close();
+    delete target_socket;
+
+    LOG_INFO("PostgreSQLPipeline::end");
 #ifdef WINDOWS_OS
     return 0;
 #else
