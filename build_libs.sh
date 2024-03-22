121aebc45c61b4c750748794ff7191063582c406
vaisakh <mail.vaisakhvm@gmail.com>
2024-03-22 19:01:13 +0530

    feat: adds asabru-ui

--- build_libs.sh
+++ build_libs.sh
@@ -11,33 +11,41 @@ list_of_directories=(
   ./lib/asabru-parsers
   ./lib/asabru-handlers
   ./lib/asabru-client
+  ./lib/asabru-ui
 )
 # For each subdirectory
 # for dir in $(find ./lib -maxdepth 1 -type d)
 for dir in "${list_of_directories[@]}"; do
   # Avoid the root directory itself ('.').
   if [[ $dir != "./lib" ]]; then
-
-    cd $dir
-    rm -rf ./build
-    # If the build directory doesn't exist, create it.
-    if [[ ! -d build ]]; then
-      mkdir build
-    fi
-
-    # Move into the build directory, run cmake and make.
-    cd build || exit
-    make clean
-
-    if [[ $dir == "./lib/libuv" ]]; then
-      cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTING=OFF # -DCMAKE_BUILD_TYPE=Debug
+    if [[ $dir == "./lib/asabru-ui" ]]; then
+      cd $dir
+      npm install
+      npm run build
+      # Go back to the root directory
+      cd $root_dir || exit
     else
-      cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON # -DCMAKE_BUILD_TYPE=Debug
-    fi
+      cd $dir
+      rm -rf ./build
+      # If the build directory doesn't exist, create it.
+      if [[ ! -d build ]]; then
+        mkdir build
+      fi
 
-    make
-    # Go back to the root directory
-    cd $root_dir || exit
+      # Move into the build directory, run cmake and make.
+      cd build || exit
+      make clean
+
+      if [[ $dir == "./lib/libuv" ]]; then
+        cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTING=OFF # -DCMAKE_BUILD_TYPE=Debug
+      else
+        cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON # -DCMAKE_BUILD_TYPE=Debug
+      fi
+
+      make
+      # Go back to the root directory
+      cd $root_dir || exit
+    fi
   fi
 done
 
