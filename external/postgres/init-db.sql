310b018528649fa25eac2d51dfe37a369dbdc625
Midhun A Darvin <midhunadarvin@gmail.com>
2024-03-30 19:46:08 +0530

    feat: add mongodb wire level packet decoding

---
+++ external/postgres/init-db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE car_prices (
+    year smallint,
+    make varchar,
+    model varchar,
+    trim varchar,
+    body varchar,
+    transmission varchar,
+    vin varchar,
+    state varchar,
+    condition smallint,
+    odometer integer,
+    color varchar,
+    interior varchar,
+    seller varchar,
+    mmr integer,
+    sellingprice integer,
+    saledate timestamp
+);
